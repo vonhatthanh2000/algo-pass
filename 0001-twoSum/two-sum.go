0ecb9e3ddbec833a93d4fa748f5c393ebb474213
thanhvo <nthanhatee@gmail.com>
2025-02-07 13:04:45 +0700

    first step: two sum by go

---
+++ 0001-twoSum/two-sum.go
@@ -0,0 +1,16 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	arrMap := make(map[int]int)
+
+	for i, num := range nums {
+		expected := target - num
+
+		if idx, exist := arrMap[expected]; exist {
+			return []int{idx, i}
+		}
+
+		arrMap[num] = i
+	}
+	return []int{}
+}
