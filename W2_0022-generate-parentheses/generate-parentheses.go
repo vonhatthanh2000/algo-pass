f2bfcbf296360519ed6c26b7dc1d5ef1e0521e1d
thanhvodev <nthanhatee@gmail.com>
2025-02-24 21:16:25 +0700

    0022 by go, ts

---
+++ W2_0022-generate-parentheses/generate-parentheses.go
@@ -0,0 +1,20 @@
+func generateParenthesis(n int) []string {
+	var result []string
+	var backtrack func(current string, open int, close int)
+
+	backtrack = func(current string, open int, close int) {
+		if len(current) == 2*n {
+			result = append(result, current)
+			return
+		}
+		if open < n {
+			backtrack(current+"(", open+1, close)
+		}
+		if close < open {
+			backtrack(current+")", open, close+1)
+		}
+	}
+
+	backtrack("", 0, 0)
+	return result
+}
---
+++ W2_0022-generate-parentheses/generate-parentheses.ts
@@ -0,0 +1,20 @@
+function generateParenthesis(n: number): string[] {
+  const result: string[] = [];
+
+  const backtrack = (current: string, open: number, close: number) => {
+    if (current.length === 2 * n) {
+      result.push(current);
+      return;
+    }
+
+    if (open < n) {
+      backtrack(current + "(", open + 1, close);
+    }
+    if (close < open) {
+      backtrack(current + ")", open, close + 1);
+    }
+  };
+
+  backtrack("", 0, 0);
+  return result;
+}
